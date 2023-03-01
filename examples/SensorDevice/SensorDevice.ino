af0604882c1c78b5934684ce988ed5b55f013fd7
dltoth <119793952+dltoth@users.noreply.github.com>
2023-03-01 11:22:18 -0500

    Add files via upload

--- examples/SensorDevice/SensorDevice.ino
+++ examples/SensorDevice/SensorDevice.ino
@@ -1,13 +1,30 @@
-
 /**
- *  Boilerplate Test Harness
+ * 
+ *  UPnPDevice Library
+ *  Copyright (C) 2023  Daniel L Toth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published 
+ *  by the Free Software Foundation, either version 3 of the License, or any 
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  The author can be contacted at dan@leelanausoftware.com  
+ *
  */
 
 #include "SimpleSensor.h"
 #include "SensorWithConfig.h"
 
-#define AP_SSID "MySSID"
-#define AP_PSK  "MYPSK"
+#define AP_SSID "My_SSID"
+#define AP_PSK  "MY_PSK"
 #define SERVER_PORT 80
 
 #ifdef ESP8266
@@ -28,7 +45,8 @@ WebContext       ctx;
 WebContext*      c   = &ctx;
 
 RootDevice       root;
-SimpleSensor     s;
+SensorWithConfig s;
+// SensorWithConfig swc;
 
 void setup() {
   Serial.begin(115200);
@@ -49,7 +67,7 @@ void setup() {
   ctx.setup(svr,WiFi.localIP(),SERVER_PORT);
   Serial.printf("Web Server started on %s:%d/\n",ctx.getLocalIPAddress().toString().c_str(),ctx.getLocalPort());
 
-  root.addDevice(&s);
+  root.addDevices(&s);
   root.setDisplayName("Sensor Test");
   root.setTarget("root");  
   root.setup(&ctx);
@@ -59,10 +77,25 @@ void setup() {
  * Print UPnP Info about RootDevice, Services, and embedded Devices
  * 
  */
-  UPnPDevice::printInfo(&root);
+  printInfo(&root);
+  for(int i=0;i<root.numDevices(); i++) {printInfo(root.device(i));}
   
 }
 
 void loop() {
   server.handleClient();
+}
+
+void printInfo(UPnPDevice* d) {
+  Serial.printf("%s:\n   UUID: %s\n   Type: %s\n",d->getDisplayName(),d->uuid(),d->getType());
+  char buffer[128];
+  d->location(buffer,128,WiFi.localIP());
+  Serial.printf("   Location is %s\n",buffer);
+  Serial.printf("   %s Services:\n",d->getDisplayName());
+  for(int i=0; i<d->numServices(); i++) {
+    UPnPService* s = d->service(i);
+    buffer[0] = '\0';
+    s->location(buffer,128,WiFi.localIP());
+    Serial.printf("      %s:\n         Type: %s\n         Location is %s\n",s->getDisplayName(),s->getType(),buffer);
+  } 
 }
