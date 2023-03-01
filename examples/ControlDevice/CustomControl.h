f0c0129a4d6ff8c175cf3192930e1f0f33c14da1
dltoth <119793952+dltoth@users.noreply.github.com>
2023-03-01 11:21:32 -0500

    Add files via upload

--- examples/ControlDevice/CustomControl.h
+++ examples/ControlDevice/CustomControl.h
@@ -1,9 +1,27 @@
 /**
  * 
+ *  UPnPDevice Library
+ *  Copyright (C) 2023  Daniel L Toth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published 
+ *  by the Free Software Foundation, either version 3 of the License, or any 
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  The author can be contacted at dan@leelanausoftware.com  
+ *
  */
 
-#ifndef CUSTOMCONTROL_H
-#define CUSTOMCONTROL_H
+#ifndef CONTROLTEST_H
+#define CONTROLTEST_H
 
 #include <CommonProgmem.h>
 #include "Control.h"
